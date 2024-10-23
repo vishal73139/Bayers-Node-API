4d32f1519aef75a7db965e6f1f8d496e757b9e6d
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2024-10-23 17:49:59 +0530

    Adding Constants Variables

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -2,13 +2,14 @@ const INVALID_PASSWORD = 'Error : Invalid Username or Password.';
 const INVALID_EMAIL = 'Email is not valid!';
 const INVALID_NAME_ERROR = 'Name is required!';
 const USER_ALERADY_ERROR = 'User already exists!';
-const USER_REGISTER_SUCCESS = 'User was registered successfully!';
+
 const UNABLE_TO_REGISTER_ERROR = 'Unable to register user.';
 
 const DOCTOR_ALERADY_EXISTS = 'A doctor with this email already exists';
 const PASSWORD_ERROR = 'Password must be at least 8 characters!';
 
 //SUCCESS
+const USER_REGISTER_SUCCESS = 'User was registered successfully!';
 const PROFILE_UPDATE_SUCCESSFULLY = 'Profile updated successfully';
 const DOCTOR_ADD_SUCCESSFULLY = 'Doctor add successfully';
 
@@ -23,6 +24,7 @@ module.exports = {
 	UNABLE_TO_REGISTER_ERROR,
 
 	// SUCCESS
+	USER_REGISTER_SUCCESS,
 	PROFILE_UPDATE_SUCCESSFULLY,
 	DOCTOR_ADD_SUCCESSFULLY
 };
